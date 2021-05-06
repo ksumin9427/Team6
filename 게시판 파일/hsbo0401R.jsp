4305d2a12a425f72c6d7468267b9a168b400a651
ksumin9427 <82130259+ksumin9427@users.noreply.github.com>
2021-05-06 23:35:05 +0900

    0506 게시판 김수민

---
+++ 게시판 파일/hsbo0401R.jsp
@@ -0,0 +1,226 @@
+<%@page import="java.text.SimpleDateFormat"%>
+<%@page import="java.sql.Timestamp"%>
+<%@page import="team6.*"%>
+<%@page import="team6.BoardDBBean"%>
+<%@page import="java.util.ArrayList"%>
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+	pageEncoding="EUC-KR"%>
+<%
+	String pageNum = request.getParameter("pageNum");
+if (pageNum == null) {
+	pageNum = "1";
+}
+
+BoardDBBean db = BoardDBBean.getInstance();
+ArrayList<BoardBean> boardList = db.listBoard(pageNum);
+int i, bs_no, bs_hit, bs_level;
+String bs_name, bs_title;
+Timestamp bs_date;
+
+SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+%>
+<html>
+<head>
+<meta charset="EUC-KR">
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>가산대학교 학생지원시스템</title>
+
+<style>
+
+<style>
+header {
+	position: relative;
+}
+
+section {
+	position: absolute;
+	top: 250px;
+	right: 25%;
+	width: 50%;
+	height: 50%;
+	margin: 5px;
+}
+
+
+.line{
+border-top: 1px solid #ccc ;
+border-bottom: 1px solid #ccc;
+
+}
+
+
+.writetop{
+	padding:5px;
+	text-align: right;
+	
+}
+
+table tr th {
+	background-color: #EAEAEA;
+	border-bottom: 1px solid #ccc;
+}
+
+table tr td  {
+	border-bottom: 1px solid #ccc;
+} 
+
+table tr td a {
+	text-decoration: none;
+}
+
+
+footer {
+	clear: both;
+	position: absolute;
+	bottom: 0;
+}
+</style>
+
+</head>
+<body>
+	
+	<div id="page">
+		<header>
+			<div class="top_bg">
+				<div class="top-left-header">
+					<ul>
+						<li><a href="#">HOME</a></li>
+						<li><a href="#">LOGOUT</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="logo">
+				<h1>
+					<img src="kasanlogo2.jpg" alt="KASAN UNIVERSITY"> 가산대학교
+				</h1>
+			</div>
+
+
+			<!-- <nav> -->
+			<ul class="main-ul">
+				<li><a href="#">학적</a>
+					<ul class="sub-ul">
+						<li><a href="#">학적사항</a></li>
+						<li><a href="#">개인정보수정</a></li>
+					</ul></li>
+				<li><a href="#">수업</a>
+					<ul class="sub-ul">
+						<li><a href="#">강좌조회</a></li>
+						<li><a href="#">수강신청</a></li>
+						<li><a href="#">수강신청 확인/정정</a></li>
+						<li><a href="#">시간표조회</a></li>
+					</ul></li>
+				<li><a href="#">성적</a>
+					<ul class="sub-ul">
+						<li><a href="#">현학기 성적조회</a></li>
+						<li><a href="#">전체 성적조회</a></li>
+					</ul></li>
+
+				<li><a href="#">게시판</a>
+					<ul class="sub-ul">
+						<li><a href="#">공지사항</a></li>
+						<li><a href="hsbo0401R.jsp">자유게시판</a></li>
+					</ul></li>
+			</ul>
+			<!-- </nav> -->
+		</header>
+
+		<center>
+			<div>
+			<section>
+					<div>
+					<h2>자유 게시판 목록</h2>
+					</div>
+					
+					
+					
+					<table width="800"  >
+						<tr>
+							<td align="right"><a href="hsbo0101W.jsp?pageNum=<%=pageNum%>">글쓰기</a></td>
+						</tr>
+					</table>
+
+					<table class="line"  cellspacing="0">
+						<tr>
+							<th width="40" align="center">번호</th>
+							<th width="450" align="center">제목</th>
+							<th width="120" align="center">작성자</th>
+							<th width="130" align="center">작성일</th>
+							<th width="60" align="center">조회수</th>
+						</tr>
+
+
+						<%
+							for (i = 0; i < boardList.size(); i++) {
+							BoardBean board = boardList.get(i);
+							bs_no = board.getBs_no();
+							bs_title = board.getBs_title();
+							bs_name = board.getBs_name();
+							bs_date = board.getBs_date();
+							bs_hit = board.getBs_hit();
+							bs_level = board.getBs_level();
+						%>
+
+						<td align="center"><%=bs_no%></td>
+						<td>
+							<%
+								if (bs_level > 0) {
+								for (int j = 0; j < bs_level; j++) {
+							%> &nbsp; <%
+ 	}
+ %> 					 <%
+ 	}
+ %> 					<a href="hsbo0402R.jsp?bs_no=<%=bs_no%>&pageNum=<%=pageNum%>"><%=bs_title%></a>
+						</td>
+						<td align="center"><%=bs_name%></td>
+						<td align="center"><%=sdf.format(bs_date)%></td>
+						<td align="center"><%=bs_hit%></td>
+						</tr>
+						<%
+							}
+						%>
+					</table>
+				<br> <br>
+			<center>
+					<%=BoardBean.pageNumber(4)%>
+			</center>	
+					
+	</center>
+			
+				
+		
+			
+	</section>
+
+
+	<footer>
+		<ul>
+			<li>부산 부산진구 중앙대로 688</li>
+			<li>TEL : 051-912-1000</li>
+			<li>copyrights (C) KASAN UNIVERSITY. ALL RIGHTS RESERVED</li>
+		</ul>
+	</footer>
+	</div>
+	
+	
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 게시판 파일/team6/BoardDBBean.java
@@ -0,0 +1,352 @@
+
+package team6;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import myUtil.*;
+
+public class BoardDBBean {
+	private static BoardDBBean instance=new BoardDBBean();
+	public static BoardDBBean getInstance() {
+		return instance;
+	}
+	
+	public Connection getConnection() throws Exception{
+		Context ctx=new InitialContext();
+		DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/oracle");
+		return ds.getConnection();
+	}
+	
+	public int insertBoard(BoardBean board) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		String sql="";
+		int number;
+		
+		int no = board.getBs_no();
+		int ref = board.getBs_ref();
+		int step = board.getBs_step();
+		int level = board.getBs_level();
+		
+		try {
+			con = getConnection();
+			sql="SELECT MAX(BS_NO) FROM BOARD_S";
+			pstmt=con.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			
+			
+			if(rs.next()) {				
+				number=rs.getInt(1)+1;
+				
+				System.out.println(number);
+			
+			}else {
+				number=1;
+			}
+			
+			
+			if (no != 0) {
+				sql="update board_s set bs_step = bs_step + 1 where bs_ref=? and bs_step > ?";
+				pstmt=con.prepareStatement(sql);
+				pstmt.setInt(1, ref);
+				pstmt.setInt(2, step);
+				pstmt.executeUpdate();
+				
+				step=step+1;
+				level=level+1;
+			} else {
+				ref =number;
+				step = 0;
+				level = 0; 
+			}
+			
+			sql="insert into board_s(bs_no, bs_name, bs_title, bs_content,bs_pwd, bs_date"
+					+ ", bs_ref, bs_step, bs_level) "
+					+ "values(?,?,?,?,?,?,?,?,?)";
+			
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, number);
+			System.out.println(number);
+			pstmt.setString(2, HanConv.toKor(board.getBs_name()));
+			pstmt.setString(3, HanConv.toKor(board.getBs_title()));
+			pstmt.setString(4, HanConv.toKor(board.getBs_content()));
+			pstmt.setString(5, HanConv.toKor(board.getBs_pwd()));
+			
+			pstmt.setTimestamp(6, board.getBs_date());
+
+			pstmt.setInt(7, ref);
+			pstmt.setInt(8, step);
+			pstmt.setInt(9, level);
+			
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if (pstmt != null) pstmt.close();
+				if (con != null) con.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return 1;
+	}
+	
+	public ArrayList<BoardBean> listBoard(String pageNumber){
+		Connection con=null;
+		Statement stmt=null;
+		ResultSet rs=null;
+		ResultSet pageSet=null;
+		int dbCount=0;
+		int absoultePage=1;
+		
+		
+		ArrayList<BoardBean> boardList = new ArrayList<BoardBean>();
+		
+		try {
+			con = getConnection();
+			
+			
+			
+			stmt = con.createStatement();
+			pageSet=stmt.executeQuery("select count(bs_no) from board_s "); //글번호의 개수
+		
+			if (pageSet.next()) {
+				dbCount=pageSet.getInt(1);
+				pageSet.close();
+				stmt.close();
+			}
+			if (dbCount % BoardBean.pageSize == 0) {
+				 BoardBean.pageCount=(dbCount / BoardBean.pageSize) ; 
+			} else {
+				BoardBean.pageCount=(dbCount / BoardBean.pageSize) +1; 
+				
+			}
+			
+			
+			if (pageNumber != null) { 
+				BoardBean.pageNum= Integer.parseInt(pageNumber);
+				 absoultePage = ( (BoardBean.pageNum-1) * BoardBean.pageSize ) + 1;
+			}
+			
+			
+			
+			stmt=con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE); 
+			
+			String sql="select * from board_s order by bs_ref desc, bs_step asc";
+			rs = stmt.executeQuery(sql);
+			
+			if (rs.next()) {
+				rs.absolute(absoultePage);
+				int count=0;
+				while (count < BoardBean.pageSize) {
+					BoardBean board=new BoardBean();
+					board.setBs_no(rs.getInt(1));
+					board.setBs_name(rs.getString(2));
+					
+					board.setBs_title(rs.getString(3));
+					board.setBs_content(rs.getString(4));
+					board.setBs_pwd(rs.getString(5));
+					board.setBs_date(rs.getTimestamp(6));
+					board.setBs_hit(rs.getInt(7));
+					
+					board.setBs_ref(rs.getInt(8));
+					board.setBs_step(rs.getInt(9));
+					board.setBs_level(rs.getInt(10));
+					
+					boardList.add(board);
+					
+					if (rs.isLast()) {
+						break;
+					} else {
+						rs.next();
+					}
+					count++;
+					
+				}
+				
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if (rs != null) rs.close();
+				if (stmt != null) stmt.close();
+				if (con != null) con.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return boardList;
+	}
+	
+	public BoardBean getBoard(int bsno, boolean hitadd) {
+		Connection con=null;
+		PreparedStatement pstmtUp=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		String sql="";
+		BoardBean board=null;
+		
+		try {
+			con = getConnection();
+			
+			if (hitadd==true) {
+				sql="update board_s set bs_hit=bs_hit+1 where bs_no=?";
+				pstmtUp = con.prepareStatement(sql);
+				pstmtUp.setInt(1, bsno);
+				pstmtUp.executeUpdate();
+				pstmtUp.close();
+			}
+			
+			sql="select * from board_s where bs_no=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, bsno);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				board=new BoardBean();
+				board.setBs_no(rs.getInt(1));
+				board.setBs_name(rs.getString(2));
+				board.setBs_title(rs.getString(3));
+				board.setBs_content(rs.getString(4));
+				board.setBs_pwd(rs.getString(5));
+				board.setBs_date(rs.getTimestamp(6));
+				board.setBs_hit(rs.getInt(7));
+				board.setBs_ref(rs.getInt(8));
+				board.setBs_step(rs.getInt(9));
+				board.setBs_level(rs.getInt(10));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if (rs != null) rs.close();
+				if (pstmt != null) pstmt.close();
+				if (con != null) con.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return board;
+	}
+	
+	public int deleteBoard(int bs_no, String bs_pwd) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		String sql="";
+		int re=-1;
+		String pwd="";
+		
+		try {
+			con = getConnection();
+			sql="select bs_pwd from board_s where bs_no=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, bs_no);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				pwd = rs.getString(1);
+				
+				if (!pwd.equals(bs_pwd)) {
+					re=0;
+				}else {
+					sql="delete board_s where bs_no=?";
+					pstmt = con.prepareStatement(sql);
+					pstmt.setInt(1, bs_no);
+					pstmt.executeUpdate();
+					
+					re=1;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if (rs != null) rs.close();
+				if (pstmt != null) pstmt.close();
+				if (con != null) con.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return re;
+	}
+	
+	public int editBoard(BoardBean board) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		String sql="";
+		int re=-1;
+		String pwd="";
+		
+		try {
+			con = getConnection();
+			sql="select bs_pwd from board_s where bs_no=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, board.getBs_no());
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				pwd = rs.getString(1);
+				
+				if (!pwd.equals(board.getBs_pwd())) {
+					re=0;
+				}else {
+					sql="update board_s set bs_name=?, bs_title=?, bs_content=? where bs_no=?";
+					pstmt = con.prepareStatement(sql);
+					pstmt.setString(1, HanConv.toKor(board.getBs_name()));
+			
+					pstmt.setString(2, HanConv.toKor(board.getBs_title()));
+					pstmt.setString(3, HanConv.toKor(board.getBs_content()));
+					pstmt.setInt(4, board.getBs_no());
+					pstmt.executeUpdate();
+					re=1;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if (rs != null) rs.close();
+				if (pstmt != null) pstmt.close();
+				if (con != null) con.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return re;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
