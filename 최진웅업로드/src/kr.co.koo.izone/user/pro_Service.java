8de133c4794135bad3cde5df0db7a5373fe904e4
choijinwoong <qwaszx3677@naver.com>
2021-05-22 17:44:51 +0900

    삭제/20210522

--- 최진웅업로드/src/kr.co.koo.izone/user/pro_Service.java
+++
@@ -1,9 +0,0 @@
-package kr.co.koo.izone.user;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface pro_Service {
-	
-	void execute(HttpServletRequest request, HttpServletResponse response);
-}
